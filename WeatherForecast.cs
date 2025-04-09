c41beeca19ed01e07232955fefd01a1a6e7cd7d4
lopica <ducanhedison@gmail.com>
2025-04-09 17:14:55 +0700

    Add project files.

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Asp_Net_EF_Core_1
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
